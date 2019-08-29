8a9b3fd2ca381833e37d32e477405cbf42c835e7
minsung <bs97482@naver.com>
2019-08-29 10:05:09 +0900

    minsungTest

---
+++ lastproject222/bin/src/main/resources/static/projecttable.sql
@@ -0,0 +1,12 @@
+select * from tab;
+select * from user_sequences;
+
+drop sequence user_sequences; 
+
+select *from userdata
+select *from BUCKET
+
+select * from PRODUCT_IMG
+select * from product
+drop table logindata
+select * from options
