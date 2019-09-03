04c4af751f31b8a14e9bb0fcd388857a816aaea2
jjangyy24 <jym971024@naver.com>
2019-09-03 11:34:56 +0900

    0903커밋테스트

--- lastproject222/src/main/java/com/encore/controller/ProductController.java
+++ lastproject222/src/main/java/com/encore/controller/ProductController.java
@@ -2,11 +2,9 @@ package com.encore.controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -18,10 +16,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
@@ -30,7 +26,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.encore.domain.Bucket;
 import com.encore.domain.Option;
-import com.encore.domain.Order;
 import com.encore.domain.Product;
 import com.encore.domain.ProductImg;
 import com.encore.domain.Userdata;
